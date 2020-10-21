7b7875c2b7018d7f655944db57e7c265d54a0a7d
ashley kildunne <ashleykildunne@ashleys-MBP.attlocal.net>
2020-10-20 20:24:21 -0700

    Login Working

---
+++ client/src/components/layout/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import Header from '../components/Header'
+
+export default function Layout() {
+  // const { currentUser, handleLogout } = props;
+  return (
+    <div className="App">
+      <div>Hello World</div>
+      {/* <Header
+        currentUser={currentUser}
+        handleLogout={handleLogout}
+      />
+      {props.children} */}
+    </div>
+  )
+}
