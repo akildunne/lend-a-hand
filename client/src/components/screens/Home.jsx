5172bf3e754d617121371f94fa2168742b841b18
ashley kildunne <ashleykildunne@ashleys-MBP.attlocal.net>
2020-10-21 08:42:03 -0700

    Home Page Rendering

--- client/src/components/screens/Home.jsx
+++ client/src/components/screens/Home.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
-
+import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+import { getAllCauses } from "../../services/causes";
 
 export default function Home() {
- 
+  // const [redirect, setRedirect] = useState(false);
+  const [allCauses, setAllCauses] = useState([]);
+
+  useEffect(() => {
+    const fetchCauses = async () => {
+      const causes = await getAllCauses();
+      setAllCauses(causes);
+    };
+    fetchCauses();
+  }, []);
+
   return (
     <div>
       <h3>Organizations to Support</h3>
+      {allCauses.map(cause => (
+        <div key={cause.id}>
+          <Link to={`/causes/${cause.id}`}>
+            <p>{cause.name}</p>
+          </Link>
+        </div> 
+      ))}
     </div>
-  )
+  );
 }
