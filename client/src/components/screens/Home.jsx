e686a3abd7c4119609f2d58fc97e9f61249860cf
ashley kildunne <ashleykildunne@ashleys-MBP.attlocal.net>
2020-10-22 09:32:08 -0700

    Footer Working

--- client/src/components/screens/Home.jsx
+++ client/src/components/screens/Home.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getAllCauses } from "../../services/causes";
 
-export default function Home() {
+export default function Home(props) {
   // const [redirect, setRedirect] = useState(false);
   const [allCauses, setAllCauses] = useState([]);
 
@@ -16,10 +16,6 @@ export default function Home() {
 
   return (
     <div>
-      <h1>LEND A HAND <i class="fas fa-heart"></i> <i class="fas fa-hands"></i> </h1>
-      <Link to="/login">
-        <div>Login</div>
-      </Link>
       <div>
         <h3>Organizations to Support</h3>
         {allCauses.map((cause) => (
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ Event.destroy_all
 @et_user = User.create!(username: 'ET Admin', email: 'etadmin@email.com', password: '123456')
 @sc_user = User.create!(username: 'SC Admin', email: 'scadmin@email.com', password: '123456')
 @crp_user = User.create!(username: 'CRP Admin', email: 'crpadmin@email.com', password: '123456')
-puts "#{User.count} users created"
+
 
 @blm = Cause.create!(name: 'Black Lives Matter', image: 'https://images.app.goo.gl/TeKbYG7XPHKnayZa6', user: @blm_user)
 @hrc = Cause.create!(name: 'Human Rights Campagin', image: 'https://images.app.goo.gl/2AoSxWruZGTq7UMRA', user: @hrc_user)
@@ -24,9 +24,14 @@ puts "#{User.count} users created"
 @et = Cause.create!(name: 'Everytown for Gun Safety', image: 'https://images.app.goo.gl/GFs2NxM4Kar3f3sp6', user: @et_user)
 @sc = Cause.create!(name: 'Street Children', image: 'https://images.app.goo.gl/PC4btRebRYJzN4wn9', user: @sc_user)
 @crp = Cause.create!(name: 'Climate Reality Project', image: 'https://images.app.goo.gl/NzYTyhRazTfRW1S68', user: @crp_user)
-puts "#{Cause.count} causes created"
+
 
 @unity_march = Event.create!(name: 'Unity March', reason: 'Support in San Jose', location: 'SAP Center, San Jose', age_group: '18+', cause: @blm, user: @blm_user)
-puts "#{Event.count} events created"
+@phone_bank = Event.create!(name: 'Everytown Phone Bank', reason: 'Inform voters of laws in their state.', location: 'Everytown Office, San Francisco', age_group: '18+', cause: @et, user: @et_user)
+@escort = Event.create!(name: 'Planned Parenthood Volunteer Escort', reason: 'Protect people entering and leaving Planned Parenthood.', location: 'Planned Parenthood, Santa Clara', age_group: '18+', cause: @pp, user: @pp_user)
+@pride = Event.create!(name: 'San Jose Pride', reason: 'Show your support for love! Come join HRC for a day of dancing, fun and love.', location: 'Downtown San Francisco', age_group: 'all ages', cause: @hrc, user: @hrc_user)
+@food_drive = Event.create!(name: 'Street Children Food Drive', reason: 'Help fill backpacks with necessities to pass out to the local community in need.', location: 'Street Children Warehouse, San Jose', age_group: 'all ages', cause: @sc, user: @sc_user)
+@climate_talk = Event.create!(name: 'Climate Change Talk', reason: 'Inform voters of what they can do locally to push for real changes in government regarding climate.', location: '5th Avenue Park, Downtown San Jose', age_group: '18+', cause: @crp, user: @crp_user)
+
 
 
