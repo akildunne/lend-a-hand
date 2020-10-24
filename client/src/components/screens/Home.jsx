55b8880d8ed799d778ffedb87c8aa5f9573f790e
ashley kildunne <ashleykildunne@ashleys-MBP.attlocal.net>
2020-10-23 19:10:34 -0700

    Centered Organization Links

--- client/src/components/screens/Home.jsx
+++ client/src/components/screens/Home.jsx
@@ -1,6 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getAllCauses } from "../../services/causes";
+import styled from "styled-components";
+
+const CauseContainer = styled.div`
+  display: flex;
+  text-decoration: none;
+  justify-content: center;
+  flex-direction: column;
+
+  :hover {
+    transform: scale(1.1);
+  }
+`;
+
+const ImageContainer = styled.div`
+display: flex;
+justify-content: center;
+`;
+
+const Image = styled.img`
+  display: flex;
+  justify-content: center;
+  height: 165px;
+  width: 300px;
+`;
+
+const CauseNameContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const CauseName = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 export default function Home(props) {
   const [allCauses, setAllCauses] = useState([]);
@@ -20,8 +54,14 @@ export default function Home(props) {
         {allCauses.map((cause) => (
           <div key={cause.id}>
             <Link to={`/causes/${cause.id}`}>
-              <img src={cause.image} alt={cause.name}></img>
-              <p>{cause.name}</p>
+              <CauseContainer>
+                <ImageContainer>
+                  <Image src={cause.image} alt={cause.name}></Image>
+                </ImageContainer>
+                <CauseNameContainer>
+                  <CauseName>{cause.name}</CauseName>
+                </CauseNameContainer>
+              </CauseContainer>
             </Link>
           </div>
         ))}
